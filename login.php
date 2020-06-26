769a46b4c03944eb9e9e5a1cd5db38fb5c3dd789
rohimoz28 <rohimoz28@gmail.com>
2020-06-26 10:55:48 +0700

    buat login view

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+
+</head>
+
+<body>
+    <h1>Halaman Login</h1>
+    <ul>
+        <form action="" method="POST">
+            <li>
+                <label>
+                    Username
+                    <input type=text name=username>
+                </label>
+            </li>
+            <li>
+                <label>
+                    Password
+                    <input type=password name=password>
+                </label>
+            </li>
+            <li>
+                <button type="submit" name="login">Masuk</button>
+            </li>
+        </form>
+    </ul>
+</body>
+
+</html>
